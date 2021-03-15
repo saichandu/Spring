167895d01bb06e3488c8218e7e4f987f3f1aa146
saichandu <avvarusaichandu@gmail.com>
2021-03-15 17:46:55 +0530

    config

--- ConfigServer/src/main/java/com/movie/catalog/service/ConfigServerApplication.java
+++ ConfigServer/src/main/java/com/movie/catalog/service/ConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.movie.catalog.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
